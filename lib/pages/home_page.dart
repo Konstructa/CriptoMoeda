2ae79f2d6fb20c080c28d65527be44ef57c927df
Milena Elen <elenprojects7@gmail.com>
2022-12-15 15:55:50 -0300

    FEAT: create pageView and bottomNavigation

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/pages/moedas_page.dart';
+
+import 'favoritas_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int paginaAtual = 0;
+  late PageController pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: paginaAtual);
+  }
+
+  setPaginaAtual(pagina) {
+    setState(() {
+      paginaAtual = pagina;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pc,
+        onPageChanged: setPaginaAtual,
+        children: const [MoedasPage(), FavoritasPage()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: paginaAtual,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todas'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favoritas'),
+        ],
+        onTap: (pagina) {
+          pc.animateToPage(pagina,
+              duration: const Duration(milliseconds: 400), curve: Curves.ease);
+        },
+      ),
+    );
+  }
+}
