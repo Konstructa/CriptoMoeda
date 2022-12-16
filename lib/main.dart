91c9405132c7a379bffeea475de4bf21b22707a4
Milena Elen <elenprojects7@gmail.com>
2022-12-16 16:15:03 -0300

    FEAT: add Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/configs/app_settings.dart';
+import 'package:hello_world/repositories/conta_repository.dart';
 import 'package:hello_world/repositories/favoritas_repository.dart';
 import 'package:provider/provider.dart';
+import 'configs/hive_config.dart';
 import 'meu_aplicativo.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FavoritasRepository(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => AppSettings(),
-        ),
-      ],
-      child: const MeuAplicativo(),
-    )
-    
-  );
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
 
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => ContaRepository(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => FavoritasRepository(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => AppSettings(),
+      ),
+    ],
+    child: const MeuAplicativo(),
+  ));
+}
