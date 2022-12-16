d2965129897b56c9df5d7657085a1c805e39db7d
Milena Elen <elenprojects7@gmail.com>
2022-12-16 13:30:30 -0300

    FEAT: add MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/configs/app_settings.dart';
 import 'package:hello_world/repositories/favoritas_repository.dart';
 import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => FavoritasRepository(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => FavoritasRepository(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => AppSettings(),
+        ),
+      ],
       child: const MeuAplicativo(),
     )
+    
   );
 }
 
